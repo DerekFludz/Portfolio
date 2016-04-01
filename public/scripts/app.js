674751237bd3176c62b7a5bcc0086d017509ea1e
Derek Fludzinski <thesource@Dereks-MacBook-Air.local>
2016-04-01 14:47:51 -0400

    basic cpu square sequence tested

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,6 +5,18 @@ app.controller('MainController', function(){
   var controller = this;
   this.slide = 1;
 
+  this.slide1 = function() {
+    return controller.slide = 1;
+  }
+
+  this.slide2 = function() {
+    return controller.slide = 2;
+  }
+
+  this.slide3 = function() {
+    return controller.slide = 3;
+  }
+
   this.slider = function() {
     return controller.slide +=1;
   };
--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -7,6 +7,7 @@ $(function(){
   // };
 
   ////////// PUCK GAME LOGIC //////////
+
   var startPucks = function() {
 
     var randomNumber = function(x) {
@@ -44,21 +45,100 @@ $(function(){
     $('#start-pucks').remove();
     return startPucks();
   });
+
   ////////// END PUCK GAME LOGIC //////////
 
   ////////// SIMON GAME LOGIC //////////
 
   var $streak = 0;
+  var $round = 0;
 
   var $cpuMoves = [];
   var $playerMoves = [];
 
+  var randomNum = function() {
+    return Math.floor(Math.random() * (4 - 1 + 1)) + 1;
+  };
 
+  var startSimon = function() {
+    $cpuMoves = [];
+    $playerMoves = [];
+    $round = 0;
+    $streak = 0;
+    newRound();
+  };
+
+  var newRound = function() {
+    $round += 1;
+    $cpuMoves.push(randomNum());
+    animateBoard($cpuMoves);
+  };
+
+  var animateBoard = function(x) {
+    var i = 0;
+    var interval = setInterval(function() {
+      brightenSquare(x[i]);
+      i++;
+      if (i >= $cpuMoves.length) {
+        clearInterval(interval);
+        // boardOn();
+        newRound();
+      };
+    }, 1000);
+  };
+
+  // animate squares
+  var brightenSquare = function(x) {
+    if (x == 1) {
+      $('#1').addClass('lit');
+      setTimeout(function(){
+        $('#1').removeClass('lit');
+      }, 200);
+    } else if (x == 2) {
+      $('#2').addClass('lit');
+      setTimeout(function(){
+        $('#2').removeClass('lit');
+      }, 200);
+    } else if (x == 3) {
+      $('#3').addClass('lit');
+      setTimeout(function(){
+        $('#3').removeClass('lit');
+      }, 200);
+    } else if (x == 4) {
+      $('#4').addClass('lit');
+      setTimeout(function(){
+        $('#4').removeClass('lit');
+      }, 200);
+    }
+  };
+
+  // allow user interaction
+  // var boardOn = function() {
+  //
+  // }
+
+  // disable user interaction
+  // var boardOff = function() {
+  //
+  // }
+
+  $(document).on('click', '#start-simon', function() {
+    $('#start-simon').remove();
+    return startSimon();
+  });
 
   // random computer move
-  cpuMove = function() {
-    return
-  }
+
+  // pushed into move array
+
+  // player move
+
+  // check for match with move array
+
+  // restart round
+
+  // on 10 stop and proceed to next page
+
 
   ////////// END SIMON GAME LOGIC //////////
 
